db77ec0193f43f36c7ffc0d41bce87d0e2e6f9c5
imyzhuk <imyzhuk@gmail.com>
2021-02-28 14:50:10 +0300

    feat: add 1 function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        array.push(bracketsConfig[i].join(""));
+    }
+
+    function checking(someStr) {
+        let checkStr = someStr;
+
+        for (let i = 0; i < array.length; i++) {
+            checkStr = checkStr.replace(array[i], "");
+        }
+
+        if (someStr === checkStr) {
+            return checkStr;
+        } else {
+            return checking(checkStr);
+        }
+    }
+    let result = checking(str);
+    if (result === "") {
+        return true;
+    } else {
+        return false;
+    }
+};
